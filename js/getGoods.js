e336370e642bf93945b2f21af7b26f553f7d7dee
shakhriyor348 <62033235+shakhriyor348@users.noreply.github.com>
2021-11-24 23:49:20 +0300

    firstCommit

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,16 +1,65 @@
 const getGoods = () => {
-    const links = document.querySelectorAll('.navigation-link') 
+    const links = document.querySelectorAll('.navigation-link')
+
+    renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ''
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+           <div class="goods-card">
+           <span class="label ${good.label ? null : 'd-none'} ">${good.label}</span>
+           <img src="db/${good.img}" alt="${good.name}: Hoodie" class="goods-image">
+           <h3 class="goods-title">${good.name}</h3>
+           <p class="goods-description">${good.description}</p>
+           <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+               <span class="button-price">$${good.price}</span>
+           </button>
+            </div>
+            
+           `
+
+           goodsContainer.appendChild(goodBlock)
+        })
+    }
+
+
+
     links.forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault()
-            getData()
+            const linkValue = link.textContent
+            const category = link.dataset.field
+
+
+            getData(linkValue, category)
         })
+
+        if (localStorage.getItem('data') && window.location.pathname === '/goods.html') {
+            renderGoods(JSON.parse(localStorage.getItem('data')))
+        }
     })
 
-    const getData = () => {
+    const getData = (value, category) => {
         fetch('https://willberries-48fee-default-rtdb.firebaseio.com/db.json')
             .then(res => res.json())
-            .then(data => localStorage.setItem('data', JSON.stringify(data)))
+            .then(data => {
+                const arr = category ? data.filter((item) => item[category] === value) : data
+
+                localStorage.setItem('data', JSON.stringify(arr))
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(arr)
+                }
+
+            })
     }
 }
 
--- js/search.js
+++ js/search.js
@@ -2,9 +2,55 @@ const search = () => {
     const input = document.querySelector('.search-block > input')
     const searchBtn = document.querySelector('.search-block > button')
 
+    
+    renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ''
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+           <div class="goods-card">
+           <span class="label ${good.label ? null : 'd-none'} ">${good.label}</span>
+           <img src="db/${good.img}" alt="${good.name}: Hoodie" class="goods-image">
+           <h3 class="goods-title">${good.name}</h3>
+           <p class="goods-description">${good.description}</p>
+           <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+               <span class="button-price">$${good.price}</span>
+           </button>
+            </div>
+            
+           `
+
+           goodsContainer.appendChild(goodBlock)
+        })
+    }
+
+    
+    const getData = (value) => {
+        fetch('https://willberries-48fee-default-rtdb.firebaseio.com/db.json')
+            .then(res => res.json())
+            .then(data => { 
+                const arr = data.filter(good => good.name.toLowerCase().includes(value.toLowerCase()))
+
+                localStorage.setItem('data', JSON.stringify(data))
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(arr)
+                }
+
+            })
+    }
+
 
     searchBtn.addEventListener('click', () => {
-        console.log(input.value);
+        getData(input.value)
     })
 }
 
